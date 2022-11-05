5c74a5b0b7419d7c9a41e2bc655f69f53f761b02
ANshSUt <116484971+ANshSUt@users.noreply.github.com>
2022-11-05 14:08:43 +0530

    Add files via upload

---
+++ P-117/CreateVideo.py
@@ -0,0 +1,19 @@
+import os
+import cv2
+path = "Image/"
+Images = [ ]
+os.listdir(path)
+os.splitext(file)
+if ext in ['.gif', '.png', '.jpg', '.jpeg','.jfif']:
+     file_name = path+"/"+ file
+     print(file_name) 
+frame = cv2.imread(images[0])
+frame.shape
+width, height & Channels
+size = (width,height)
+print(size) 
+out
+cv2,VideoWriter()
+video name = Project.avi
+cv2.imread() 
+
